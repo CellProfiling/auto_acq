8e9cd82261e3a71843dedeb094fed67abff89adf
MartinHjelmare <marhje52@kth.se>
2014-08-19 00:28:01 +0200

    Ready for testing

--- change_coords.py
+++ change_coords.py
@@ -1,6 +1,5 @@
 import csv
 from lxml import etree
-import re
 
 working_dir = sys.argv[1]
 coord_file = sys.argv[2]
@@ -21,8 +20,8 @@ def create_dict(input_list, output_dict, key, value):
 dxs = {}
 dys = {}
 
-dxs = create_dict(coords, dxs, "well", "dx")
-dys = create_dict(coords, dys, "well", "dy")
+dxs = create_dict(coords, dxs, "fov", "dx")
+dys = create_dict(coords, dys, "fov", "dy")
 
 xml_doc = etree.parse(xml_input)
 
@@ -30,19 +29,17 @@ enable = etree.parse('/home/martin/Dev/auto_acq/enable.xsl')
 
 t_enable = etree.XSLT(enable)
 
-# For each pos in green_unique, copy job, change gain in channels,
-# assign the job if value of green_unique matches any value in green
-
+# For each fov in dxs, change x/y coordinates
 for k, v in dxs.iteritems():
     # change xcoord in well k
     wellx = str(int(k[1:3])+1)
     welly = str(int(k[6:8])+1)
+    fieldx = str(int(k[11:13])+1)
+    fieldy = str(int(k[16:18])+1)
     dx = v
     dy = dys[k]
-    for i in range(2): # 2x2 fields
-        for j in range(2):
-            xml_doc = t_enable(xml_doc, WELLX=wellx, WELLY=welly,
-                    FIELDX=str(j+1), FIELDY=str(i+1), DX=str(dx), DY=str(dy))
+    xml_doc = t_enable(xml_doc, WELLX=wellx, WELLY=welly, FIELDX=fieldx,
+            FIELDY=fieldy, DX=str(dx), DY=str(dy))
 
 # Save the xml to file
 xml_doc.write(xml_input[0:-4]+'_new.xml', pretty_print=False)
