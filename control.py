c6fda901a57d34019b422e95e54ff77457007810
MartinHjelmare <marhje52@kth.se>
2015-01-16 20:15:31 +0100

    Continuing

--- control.py
+++ control.py
@@ -8,42 +8,60 @@ import subprocess
 import re
 import csv
 import time
+from PIL import Image
+from lxml import etree
 
-# Change std_dir to be created from std_well, 10x, 40x
 # Make different imaging_dir? 10x, 40x etc.
 
 def usage():
     """Usage function to help user start the script"""
-    print 'Usage: '+sys.argv[0]+' -i <dir> [option]'
+    print("""Usage: """+sys.argv[0]+""" -i <dir> [options]
+    
+    Options and arguments:
+    -h, --help                  : show the usage information
+    -i <dir>, --input=<dir>     : set imaging directory
+    --wdir=<dir>                : set working directory
+    --std=<well>                : set standard well
+    --firstgain=<gain_file>     : set first initial gains file
+    --secondgain=<gain_file>    : set second initial gains file
+    --finwell=<well>            : set final well
+    --finfield=<field>          : set final field""")
 
 def main(argv):
     try:
         opts, args = getopt.getopt(argv, 'hi:', ['help',
                                                  'input=',
                                                  'wdir=',
-                                                 'standard=',
+                                                 'std=',
                                                  'firstgain=',
                                                  'secondgain=',
                                                  'finwell=',
                                                  'finfield='
                                                  ])
-    if not opts:
-        print 'No options supplied'
-        usage()
+    #if not opts:
+    #    print 'No options supplied'
+    #    usage()
     except getopt.GetoptError,e:
         print e
         usage()
         sys.exit(2)
-
+    
+    imaging_dir = None
+    working_dir = None
+    std_well = None
+    first_initialgains_file = None
+    sec_initialgains_file = None
+    last_well = None
+    last_field = None
     for opt, arg in opts:
         if opt in ('-h', '--help'):
             usage()
-            sys.exit(2)
+            sys.exit()
         elif opt in ('-i', '--input'):
             imaging_dir = arg
         elif opt in ('--wdir'):
             working_dir = arg
-        elif opt in ('--standard'):
+        elif opt in ('--std'):
             std_well = arg #U00V00
         elif opt in ('--firstgain'):
             first_initialgains_file = arg
@@ -62,12 +80,6 @@ def main(argv):
 #sec_initialgains_file = sys.argv[6]
 #last_well = sys.argv[7] #U00V00
 #last_field = sys.argv[8] #X00Y00
-first_r_script = working_dir+"gain.r"
-sec_r_script = working_dir+"gain_change_objectives.r"
-#sec_std_dir = 
-
-path_to_fiji = "/opt/Fiji/ImageJ-linux64"
-imagej_macro = working_dir+"do_max_proj_and_calc_histo_arg.ijm"
 
 if __name__ =='__main__':
     main(sys.argv[1:])
@@ -110,48 +122,75 @@ def strip_fun(files, filebases, _wells, _fields, _match_string):
 def parent_dir(p):
     """Return parent directory of p"""
     return os.path.abspath(os.path.join(p, os.pardir))
+    
+def process_output(dict_list, well_list, i, output):
+    dict_list.append({'well': well_list[i],
+                      'green': output.split()[0],
+                      'blue': output.split()[1],
+                      'yellow': output.split()[2],
+                      'red': output.split()[3]
+                      })
+    return dict_list
 
+def write_csv(path, dict_list):
+    with open(path, 'wb') as f:
+        keys = ['well', 'green', 'blue', 'yellow', 'red']
+        w = csv.DictWriter(f, keys)
+        w.writeheader()
+        w.writerows(dict_list)
+
+first_r_script = working_dir+'gain.r'
+sec_r_script = working_dir+'gain_change_objectives.r'
+path_to_fiji = '/opt/Fiji/ImageJ-linux64'
+imagej_macro = working_dir+'do_max_proj_and_calc_histo_arg.ijm'
 stage1 = True
+namespace = {'ns':'http://www.openmicroscopy.org/Schemas/OME/2008-09'}
 
 while stage1:
     images = []
     image_bases = []
     wells = []
     fields = []
-    images = search_files(images, imaging_dir, "*.tif")
+    images = search_files(images, imaging_dir, '*.tif')
     strip_fun(images, image_bases, wells, fields, 'C\d\d.+$')
     well_paths = []
     for i in range(len(images)):
         d = parent_dir(parent_dir(images[i]))
         well = wells[i]
         field = fields[i]
-        if well == std_well:
+        im = Image.open(images[i])
+        root = etree.fromstring(im.tag[270])
+        obj_serial_no = root.xpath('//ns:Objective/@SerialNumber',
+                                   namespaces=namespace)[0]
+        if well == std_well & obj_serial_no == '11506505':
             first_std_dir = d
-        if well == last_well && field == last_field:
+        elif well == std_well:
+            sec_std_dir = d
+        if well == last_well & field == last_field:
             stage1 = False
         well_images = []
-        well_images = search_files(well_images, d, "*.tif")
+        well_images = search_files(well_images, d, '*.tif')
         csv_list = []
-        csv_list = search_files(csv_list, d, "*.csv")
+        csv_list = search_files(csv_list, d, '*.csv')
         if len(well_images) == 66 & len(csv_list) == 0:
             well_paths.append(d)
     well_paths = sorted(list(set(well_paths)))
     for well in well_paths:
         imagej_output = subprocess.check_output([path_to_fiji,
-                                                 "--headless",
-                                                 "-macro",
+                                                 '--headless',
+                                                 '-macro',
                                                  imagej_macro,
                                                  well
                                                  ])
     time.sleep(5)
 
 first_files = []
-first_files = search_files(first_files, imaging_dir, "*.csv")
+first_files = search_files(first_files, imaging_dir, '*.csv')
 
 first_std_files = []
 sec_std_files = []
-first_std_files = search_files(first_std_files, first_std_dir, "*.csv")
-sec_std_files = search_files(sec_std_files, sec_std_dir, "*.csv")
+first_std_files = search_files(first_std_files, first_std_dir, '*.csv')
+sec_std_files = search_files(sec_std_files, sec_std_dir, '*.csv')
 
 first_filebases = []
 wells = []
@@ -172,23 +211,11 @@ sec_std_filebases = sorted(list(set(sec_std_filebases)))
 first_gain_dicts = []
 sec_gain_dicts = []
 
-def process_output(dict_list, well_list, i, output):
-    dict_list.append({"well": well_list[i],
-                      "green": output.split()[0],
-                      "blue": output.split()[1],
-                      "yellow": output.split()[2],
-                      "red": output.split()[3]
-                      })
-    return dict_list
-
 # for all wells run R script
 for i in range(len(first_filebases)):
     print(first_filebases[i])
     print(wells[i])
-    # Run with "Rscript path/to/script/gain.r path/to/working/dir/
-    # path/to/histogram-csv-filebase path/to/initialgains/csv-file"
-    # from linux command line.
-    r_output = subprocess.check_output(["Rscript",
+    r_output = subprocess.check_output(['Rscript',
                                         first_r_script,
                                         imaging_dir,
                                         first_filebases[i],
@@ -197,9 +224,9 @@ for i in range(len(first_filebases)):
 
     first_gain_dicts = process_output(first_gain_dicts, wells, i, r_output)
 
-    input_gains = (""+r_output.split()[0]+" "+r_output.split()[1]+" "+
-                    r_output.split()[2]+" "+r_output.split()[3]+"")
-    r_output = subprocess.check_output(["Rscript",
+    input_gains = (''+r_output.split()[0]+' '+r_output.split()[1]+' '+
+                    r_output.split()[2]+' '+r_output.split()[3]+'')
+    r_output = subprocess.check_output(['Rscript',
                                         sec_r_script,
                                         first_std_dir,
                                         first_std_filebases[0],
@@ -214,12 +241,5 @@ for i in range(len(first_filebases)):
     
     sec_gain_dicts = process_output(sec_gain_dicts, wells, i, r_output)
 
-def write_csv(path, dict_list):
-    with open(path, 'wb') as f:
-        keys = ["well", "green", "blue", "yellow", "red"]
-        w = csv.DictWriter(f, keys)
-        w.writeheader()
-        w.writerows(dict_list)
-
-write_csv(working_dir+"first_output_gains.csv", first_gain_dicts)
-write_csv(working_dir+"sec_output_gains.csv", sec_gain_dicts)
+write_csv(working_dir+'first_output_gains.csv', first_gain_dicts)
+write_csv(working_dir+'sec_output_gains.csv', sec_gain_dicts)
