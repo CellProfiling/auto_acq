d25ebaa09b53bed8d6a53bdec0d13bbf03e7f2b5
MartinHjelmare <marhje52@kth.se>
2015-01-18 01:18:00 +0100

    Started on class-based program

---
+++ control_class.py
@@ -0,0 +1,17 @@
+class Compartment(object):
+    """A compartment on the plate. Compartments have the following properties:
+
+    Attributes:
+        name: A string representing the well's name.
+    """
+
+    def __init__(self, name):
+        """Return a Well object whose name is *name*"""
+        self.name = name
+        self.path = path
+        self.parent = parent
+        self.children = children
+        self.files = files
+        self.images = images
+        self.commands = commands
+        
