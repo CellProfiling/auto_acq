e4fb4d4cb37ada03eb9ed5ca91d63ebef1e05bbd
MartinHjelmare <marhje52@kth.se>
2015-01-19 05:50:24 +0100

    error solving, not done yet

--- control_class.py
+++ control_class.py
@@ -1,9 +1,46 @@
 import os
 import fnmatch
 import re
+from PIL import Image
+from lxml import etree
+import abc
+class Base(object):
+    """Base class
+
+    Attributes:
+        path: A string representing the path to the object.
+    """
+
+    __metaclass__ = abc.ABCMeta
+
+    def __init__(self, path):
+        self.path = path
+
+    def get_dir(self):
+        """Return current directory."""
+        return os.path.dirname(self.path)
+
+    @abc.abstractmethod
+    def get_name(self, path, regex):
+        """Get name of idtag of image. idtag can be either
+        well, job, field, slice or channel."""
+        match = re.search(regex, os.path.basename(path))
+        if match:                      
+            return match.group()
+        else:
+            print('No match')
+            return None
+
+    @abc.abstractmethod
+    def base_type(self):
+        """"Return a string representing the type of object this is."""
+        pass
 
 class Directory(Base):
     """A directory on the plate."""
+
+    #def __init__(self, path):
+    #    super(Directory, self).__init__(path)
 
     def get_children(self):
         """Return a list of child directories."""
@@ -21,7 +58,7 @@ class Directory(Base):
         file_list = []
         for root, dirnames, filenames in os.walk(self.path):
             for filename in fnmatch.filter(filenames, regex):
-            file_list.append(os.path.join(root, filename))
+                file_list.append(os.path.join(root, filename))
         return file_list
 
         #self.commands = commands
@@ -30,14 +67,14 @@ class Directory(Base):
         """"Return a string representing the type of object this is."""
         return 'directory'
 
-from PIL import Image
-from lxml import etree
-
 class My_image(Base):
     """An image.
     Use PIL Image class to open image.
     Search xml data using lxml.
     """
+
+    #def __init__(self, path):
+    #    super(My_image, self).__init__(path)
 
     namespace = {'ns':'http://www.openmicroscopy.org/Schemas/OME/2008-09'}
 
@@ -45,7 +82,8 @@ class My_image(Base):
         """Open an image and find the serial number of the objective that
         acquired the image"""
         im = Image.open(self.path)
-        root = etree.fromstring(im.tag[270])
+        serial = etree.tostring(im.tag[270], encoding='UTF-8')
+        root = etree.fromstring(serial)
         return root.xpath('//ns:Objective/@SerialNumber',
                                    namespaces=namespace)[0]
 
@@ -70,35 +108,3 @@ class My_image(Base):
     def base_type(self):
         """"Return a string representing the type of object this is."""
         return 'image'
-
-import abc
-class Base(object):
-    """Base class
-
-    Attributes:
-        path: A string representing the path to the object.
-    """
-
-    __metaclass__ = abc.ABCMeta
-
-    def __init__(self, path):
-        self.path
-
-    def get_dir(self):
-        """Return current directory."""
-        return os.path.dirname(self.path)
-
-    @abc.abstractmethod
-    def get_name(self, path, regex):
-        """Get name of idtag of image. idtag can be either
-        well, job, field, slice or channel."""
-        match = re.search(regex, os.path.basename(path))
-        if match:                      
-            return match.group()
-        else:
-            return None
-
-    @abc.abstractmethod
-    def base_type(self):
-        """"Return a string representing the type of object this is."""
-        pass
